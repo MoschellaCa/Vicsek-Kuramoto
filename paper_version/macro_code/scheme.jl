7419539657e4272c6c105450a42ca725947d0304
Carmela Moschella <carmela.moschella@univie.ac.at>
2025-12-15 23:22:44 +0100

    several changes for the working version of the code

--- paper_version/macro_code/scheme.jl
+++ paper_version/macro_code/scheme.jl
@@ -1,163 +1,252 @@
-#-------- Add the package to the current path if not installed globally -------#
-push!(LOAD_PATH,joinpath(pwd(),"src"))
-#------------------------------------------------------------------------------#
+"""
+    scheme_iter!(
+        ρ,w,u,v,
+        Δx,Δy,Δt,
+        c1,c2,λ,
+        bcond_x,bcond_y,
+        Fx,Fy,
+        method
+    )
 
-# using SOH   # import the package
-include("SOH.jl")
+One iteration of the numerical scheme which updates the data `(ρ,w,u,v)`. The scheme
+follows the methodology introduced by
 
+S. Motsch, L. Navoret, *Numerical simulations of a non-conservative hyperbolic
+system with geometric constraints describing swarming behavior*, Multiscale Model. Simul.,
+Vol. 9, No. 3, pp. 1253-1275, 2011.
 
-using DataFrames, Dates, CSV 
-# Initializes the CSV file with headers if it doesn't exist.
-# If the file exists, reads it to find the last simulation ID.
-# Returns the next simulation ID to be used.
-function initialize_csv_log(csv_path::String)::Int
-    if !isfile(csv_path)
-        # Create an empty DataFrame with the specified headers
-        empty_df = DataFrame(simulation_id =[] , κ=[] , Lx=[] , Ly=[] , Δx = [], Δy=[], Δt=[], mean_ρ=[],
-                             mean_w=[], mean_θ=[], range_ρ=[], range_w=[], range_θ=[], date = [])
-        # Write the empty DataFrame to CSV (this creates the file with headers)
-        CSV.write(csv_path, empty_df)
-        return 1  # Start simulation IDs at 1
-    else
-        println("CSV file exists at ", csv_path, ". Reading last simulation ID...")
-        # Read the existing CSV
-        try
-            existing_df = CSV.read(csv_path, DataFrame)
-            if nrow(existing_df) == 0
-                println("CSV file is empty. Starting simulation IDs at 1.")
-                return 1
+The finite volume scheme is based on the following three-step splitting:
+
+1. The conservative part
+
+    ∂ₜρ + c₁∇ₓ⋅(ρΩ) = 0,
+    ∂ₜ(ρω) + c₁∇ₓ⋅(ρωΩ) = 0,
+    ∂ₜ(ρΩ) + c₂∇ₓ⋅(ρΩ⊗Ω) + λ∇ₓρ = 0,
+
+2. The relaxation part
+
+    ε∂ₜΩ = (1-|Ω|^2)Ω
+
+3. The source term
+
+    ∂ₜρ = 0
+    ∂ₜΩ = 
+
+For the first and second steps, a simple dimensional splitting is used. The second
+step reduces to a mere normalization. The third step can also be solved explicitly
+in dimension 2.
+"""
+function scheme_iter!(
+    ρ,w,u,v,
+    Δx,Δy,Δt,
+    c1,c2,λ,
+    bcond_x,bcond_y,
+    Fx,Fy,
+    flux_ρ,flux_w,flux_u, flux_v,
+    method
+)
+    ncellx = size(ρ)[1] - 2
+    ncelly = size(ρ)[2] - 2
+
+    #===============================================================================#
+    #======================= Dimensional splitting: x-axis =========================#
+    #===============================================================================#
+
+    bad_rho_x = 0   #Count the number of nonpositive ρ (should not happen with HLLE)
+
+    #*********************** Finite volume scheme along the x-axis *****************#
+    # U = (ρ,ρw,ρu,ρv), Ω = (u,v)
+    # 1. (Compute the flux)
+    # 2. (Conservative part) U^{n+1}_i = U^{n}_i - Δt/Δx(F^n_{i+1/2} - F^n_{i-1/2})
+    # 3. (Relaxation) Ω^{n+1}_i = Ω^{n+1/2}_i/|Ω^{n+1/2}_i|
+    # 4. (Boundary conditions)
+    #*******************************************************************************#
+
+    """ Remember that flux_x gives as an output the net flux ρΩ1,ρωΩ1, ρΩ1² + λρ, ρΩ1Ω2,
+    so flux_ρ is the net flux difference ρΩ1 ecc..
+    """
+    #----------------------- 1. Compute the flux -----------------------------------#
+
+    @inbounds for j in 1:ncelly
+        @simd for i in 1:(ncellx+1)
+            F = flux_x(
+                ρ[i,j+1],ρ[i+1,j+1],
+                w[i,j+1],w[i+1,j+1],
+                u[i,j+1],u[i+1,j+1],
+                v[i,j+1],v[i+1,j+1],
+                c1,c2,λ,method
+            )
+            flux_ρ[i,j] = F[1]
+            flux_w[i,j] = F[2]
+            flux_u[i,j] = F[3]
+            flux_v[i,j] = F[4]
+        end
+    end
+
+    #----------------------- Update ------------------------------------------------#
+
+    @inbounds for j in 2:(ncelly+1)
+        for i in 2:(ncellx+1)
+
+            #------ 2. Conservative part -------------------------------------------#
+            U = (ρ[i,j] - (Δt/Δx) * (flux_ρ[i, j-1] - flux_ρ[i-1, j-1]),
+                 ρ[i,j]*w[i,j] - (Δt/Δx) * (flux_w[i, j-1] - flux_w[i-1, j-1]),
+                 ρ[i,j]*u[i,j] - (Δt/Δx) * (flux_u[i, j-1] - flux_u[i-1, j-1]),
+                 ρ[i,j]*v[i,j] - (Δt/Δx) * (flux_v[i, j-1] - flux_v[i-1, j-1]))
+
+            #------- 3. Relaxation -------------------------------------------------#
+            norm = sqrt(U[3]^2 + U[4]^2)
+
+            if U[1]<0
+                bad_rho_x += 1  # Bad ρ<0
+                ρ[i,j] = 1e-6   # Set ρ to a small value
             else
-                # Assuming simulation_id is sorted, find the maximum ID
-                last_id = maximum(existing_df.simulation_id)
-                println("Last simulation ID found: ", last_id)
-                return last_id + 1
+                ρ[i,j] = U[1]
             end
-        catch e
-            println("Error reading CSV file: ", e)
-            println("Starting simulation IDs at 1.")
-            return 1
+            if norm>1e-12   # If the norm is too small, it is treated as zero
+                u[i,j] = U[3]/norm
+                v[i,j] = U[4]/norm
+            else
+                normuv = sqrt(u[i,j]^2+v[i,j]^2)
+                u[i,j] = u[i,j]/normuv
+                v[i,j] = v[i,j]/normuv
+            end
+
+        end
+    end
+
+    # Warn the user in case of unphysical ρ
+    if bad_rho_x>0
+        println("Warning! Nonpositive densities in $bad_rho_x cells ($(bad_rho_x/(ncellx*ncelly))%) have been removed.")
+    end
+
+    #----------------------- 4. Boundary conditions --------------------------------#
+    boundary_conditions_ρwuv!(ρ,w,u,v,bcond_x,bcond_y)
+
+
+    #===============================================================================#
+    #======================= Dimensional splitting: y-axis =========================#
+    #===============================================================================#
+
+    bad_rho_y = 0 #Count the number of nonpositive ρ (should not happen with HLLE)
+
+    #----------------------- 1. Compute the flux -----------------------------------#
+    # Note: Change basis (u,v) -> (v,-u) in order to use the same function `flux_x`
+    #-------------------------------------------------------------------------------#
+
+    @inbounds for j in 1:(ncelly+1)
+        @simd for i in 1:ncellx
+            F = flux_x(
+                ρ[i+1,j],ρ[i+1,j+1],
+                w[i+1,j],w[i+1,j+1],
+                v[i+1,j],v[i+1,j+1],
+                -u[i+1,j],-u[i+1,j+1],
+                c1,c2,λ,method
+            )
+            flux_ρ[i,j] = F[1]
+            flux_w[i,j] = F[2]
+            flux_u[i,j] = -F[4]
+            flux_v[i,j] = F[3]
+        end
+    end
+
+    #----------------------- Update ------------------------------------------------#
+    @inbounds for j in 2:(ncelly+1)
+        @simd for i in 2:(ncellx+1)
+
+            #------ 2. Conservative part -------------------------------------------#
+            U = (ρ[i,j] - (Δt/Δy) * (flux_ρ[i-1, j] - flux_ρ[i-1, j-1]),
+                 ρ[i,j]*w[i,j] - (Δt/Δy) * (flux_w[i-1, j] - flux_w[i-1, j-1]),
+                 ρ[i,j]*u[i,j] - (Δt/Δy) * (flux_u[i-1, j] - flux_u[i-1, j-1]),
+                 ρ[i,j]*v[i,j] - (Δt/Δy) * (flux_v[i-1, j] - flux_v[i-1, j-1]))
+
+            #------- 3. Relaxation -------------------------------------------------#
+            norm = sqrt(U[3]^2 + U[4]^2)
+
+            if U[1]<0
+                bad_rho_y += 1  # Bad ρ<0
+                ρ[i,j] = 1e-6   # Set ρ to a small value
+            else
+                ρ[i,j] = U[1]
+            end
+            if norm>1e-12 # If the norm is too small, it is treated as zero
+                u[i,j] = U[3]/norm
+                v[i,j] = U[4]/norm
+            else
+                normuv = sqrt(u[i,j]^2+v[i,j]^2)
+                u[i,j] = u[i,j]/normuv
+                v[i,j] = v[i,j]/normuv
+            end
+
+        end
+    end
+
+    # Warn the user in case of unphysical ρ
+    if bad_rho_y>0
+        println("Warning! Nonpositive densities in $bad_rho_y cells ($(bad_rho_y/(ncellx*ncelly))%) have been removed.")
+    end
+
+    #----------------------- 4. Boundary conditions --------------------------------#
+    boundary_conditions_ρwuv!(ρ,w,u,v,bcond_x,bcond_y)
+
+
+    #===============================================================================#
+    #======================= Fractional splitting: exterior force ==================#
+    #===============================================================================#
+    
+    scheme_rotation!(ρ, w, u, v, Δt)
+    boundary_conditions_ρwuv!(ρ,w,u,v,bcond_x,bcond_y)
+    
+
+
+end
+
+"""
+    scheme_potential!(u,v,Fx,Fy,Δt,λ)
+
+Solve explicitly the source term
+
+∂ₜρ = 0
+∂ₜΩ = λP(Ω)F, F=-∇ₓV.
+
+In dimension two, the solution is
+
+Ω(t) = (cos(θ),sin(θ))ᵀ where
+
+θ(t) = ψ + 2atan(C_0 exp(-λ|F|t))
+
+with C_0 = tan((θ(0)-ψ)/2) and F = |F|(cos(ψ),sin(ψ))ᵀ.
+
+function scheme_potential!(u,v,Fx,Fy,Δt,λ)
+    ncellx = size(u)[1] - 2
+    ncelly = size(u)[2] - 2
+    @inbounds for j in 2:ncelly+1
+        for i in 2:ncellx+1
+            normF = sqrt(Fx[i,j]^2 + Fy[i,j]^2)
+            if normF > 1e-9
+                θ0 = atan(v[i,j],u[i,j])
+                ψ = atan(Fy[i,j],Fx[i,j])
+                C0 = tan((θ0-ψ)/2)
+                θ = ψ + 2 * atan(C0*exp(-λ*normF*Δt))
+                u[i,j] = cos(θ)
+                v[i,j] = sin(θ)
+            end
         end
     end
 end
-
-# Function to append a single simulation result to the CSV
-function append_simulation_result(csv_path::String, simulation_id::Int, κ, Lx, Ly, Δx, Δy, Δt, mean_ρ, 
-                                  mean_w, mean_θ, range_ρ, range_w, range_θ, date)
-    # Create a DataFrame for the new row
-    new_row = DataFrame(
-        simulation_id =[simulation_id] ,
-        κ=[κ] ,
-        Lx=[Lx] ,
-        Ly=[Ly] ,
-        Δx=[Δx] ,
-        Δy = [Δy], 
-        Δt=[Δt], 
-        mean_ρ=[mean_ρ], 
-        mean_w=[mean_w], 
-        mean_θ=[mean_θ],
-        range_ρ=[range_ρ],
-        range_w=[range_w],
-        range_θ=[range_θ],
-        date = [date]
-    )
-    
-    # Append the new row to the CSV without writing headers
-    CSV.write(csv_path, new_row, append=true)
+"""
+function scheme_rotation!(ρ, w, u, v, Δt)
+    ncellx = size(u)[1] - 2
+    ncelly = size(u)[2] - 2
+    @inbounds for j in 2:ncelly+1
+        for i in 2:ncellx+1
+            # Current angle:
+            θ0 = atan(v[i,j], u[i,j])
+            # Update angle by the local angular speed w[i,j]:
+            θ  = θ0 + w[i,j] * Δt
+            # Write back the updated velocity:
+            u[i,j] = cos(θ)
+            v[i,j] = sin(θ)
+        end
+    end
 end
-
-output_csv = "data_macro_simulation.csv"
-save_path = "/Users/moschellaca/Desktop/Vicsek-Kuramoto model/Macro_simulations/"
-date = now()
-
-
-#-------- Model parameters ----------------------------------------------------#
-# The coefficients are computed using the function `coefficients_Vicsek` in the 
-# script `toolbox.jl` for the Fokker-Planck and the BGK models. 
-#for κ in 1.0:3.0:28.0
-    κ = 50.0     # concentration parameter
-    c1,c2,λ = coefficients_Vicsek(κ)
-
-    #-------- Domain parameters ---------------------------------------------------#
-
-    # Rectangular domain of size Lx*Ly
-    Lx = 1.0   
-    Ly = 1.0
-
-    # Boundary conditions (possible choices "periodic", "Neumann", "reflecting")
-    bcond_x = "periodic"
-    bcond_y = "periodic"
-
-
-    #-------- Numerical parameters ------------------------------------------------#
-
-    # Numer of cells and spatial step
-    ncellx = 200
-    ncelly = 200
-    Δx = Lx / ncellx
-    Δy = Ly / ncelly
-
-    # Time step 
-    Δt = 0.001
-
-    # Final time
-    T = 30.
-
-    # Method ("Roe" or "HLLE")
-    method = "Roe"
-
-
-    #-------- Exterior force ------------------------------------------------------#
-
-    # If no exterior force:
-    #Fx = nothing
-    #Fy = nothing
-
-    # Otherwise define the x and y components as (ncellx+2)*(ncelly+2) matrices.
-    # See the examples in the script `init.jl`
-    #Fx, Fy = flat_quadratic_potential_force(ncellx,ncelly,Δx,Δy,Lx/3,5.)
-
-    #-------- Saving parameters ---------------------------------------------------#
-
-    should_save = false
-    simu_name = "simu"
-    should_plot = false
-    step_plot = 2   # A plot every `step_plot` iterations
-    save_video = true   
-
-    #-------- Initial conditions --------------------------------------------------#
-    mean_rho = 1.0
-    range_rho = 0.
-    mean_omega = 5.
-    range_omega = 1.
-    mean_theta = 0.
-    range_theta = 2*pi
-    ρ,w,u,v = random_init(ncellx, ncelly, mean_rho, range_rho, mean_omega, range_omega, mean_theta, range_theta,bcond_x=bcond_x,bcond_y=bcond_y)
-
-
-    #-------- Finally run the simulation ------------------------------------------#
-    Fx = nothing
-    Fy = nothing
-    run!(
-        ρ,w,u,v;
-        Lx=Lx,Ly=Ly,Δt=Δt,
-        c1=c1, c2=c2, λ=λ,
-        final_time=T,
-        bcond_x=bcond_x,bcond_y=bcond_y,
-        Fx=Fx,Fy=Fy,
-        method=method,
-        simu_name="simu",
-        should_save=should_save,save_step=1,
-        should_plot=should_plot,plot_step=2,
-        save_video=save_video,
-        range=(0.,5.0),resolution=(800,600),theme="dark",fps=60
-    );
-
-    next_sim_id = initialize_csv_log(output_csv)
-    # Append the results to the CSV file
-    append_simulation_result(output_csv, next_sim_id, κ, Lx, Ly, Δx, Δy, Δt, mean_rho, mean_omega, mean_theta, range_rho, range_omega, range_theta, date)
-
-
-#end 
-
-
-
